9b9b29479045d907e3ada306732c9d7ed1203da4
Jako <jacobvoandersen@gmail.com>
2024-02-23 09:16:53 +0100

    Add project files.

---
+++ Migrations/20240209094840_ColumnFirstName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Contoso_MVC_8_0_VS2022.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnFirstName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstMidName",
+                table: "Student",
+                newName: "FirstName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Student",
+                newName: "FirstMidName");
+        }
+    }
+}
